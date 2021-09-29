2f1cb33093c89d43e6c95f9fb52718c6f608008b
Farukh Iliyev <iliyevfarukh@gmail.com>
2021-09-29 11:29:53 +0600

    corrected mistake

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max-min)) + min;
-}
+const getRandomInt = (min, max) => {
+  if (min >= max) {
+    max = Math.ceil(max);
+    min = Math.floor(min);
+    return Math.floor(Math.random() * (max-min)) + min;
+  }
+};
 
 getRandomInt(0, 100);
 
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
+const getRandomArbitrary = (min, max) => {
+  if (min >= max) {
+    max = Math.ceil();
+    min = Math.floor();
+    return Math.random() * (max - min) + min;
+  }
+};
 
 getRandomArbitrary(0, 100);
