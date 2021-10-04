2a926f2975e6feadfd8638cdcfa64a65bbcdfe5e
Фарух <iliyevfarukh@gmail.com>
2021-10-04 22:23:53 +0600

    4.9 more details

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ const getRandomInt = (min, max) => {
   if (min < 0 || max < 0) {
     return -1;
   }
-
   if (min > max) {
     [min, max] = [max, min]; // Деструктуризация значений
   }
-
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
@@ -16,8 +14,55 @@ const getRandomArbitrary = (min, max) => {
   if (min < 0 || max < 0) {
     return -1;
   }
-
   return (Math.random() * (max - min)) + min;
 };
 
 getRandomArbitrary(7, 50);
+
+
+const author = () => {
+  const i = Math.round(Math.random() * (10 - 1) + 1);
+  const avatar =  "img/avatars/user" + i + ".png"
+};
+
+const STRINGS = [
+  'title',
+  'address',
+  'type',
+  'checkin',
+  'checkout',
+  'features',
+  'description',
+  'photos',
+]
+
+const NUMBERS = [
+  'price',
+  'rooms',
+  'guests',
+];
+
+const moreDetails = () => {
+  const randomStringIndex = _.random(0, STRINGS.length - 1);
+  const randomNumberIndex = _.random(0, NUMBERS.length - 1);
+
+  return {
+    string: STRINGS[randomStringIndex],
+    number: NUMBERS[randomNumberIndex],
+  };
+};
+
+console.log(
+  moreDetails()
+);
+
+const map = () => {
+  const location = {
+    width: {
+      lat: 35.65000, lng: 139.70000,
+    },
+    height: {
+      lat: 139.70000, lng: 139.80000,
+    }
+  }
+};
